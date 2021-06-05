9e856d41d07a4eb8aff2fc228c754028159dbcec
guido.dassori@gmail.com <guido.dassori@gmail.com>
2021-06-05 16:19:51 +0200

    attempt to add travis-ci

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name='aiodiskdb',
+    version='0.0.1',
+    url='https://github.com/mempoolco/aiodiskdb/',
+    license='MIT',
+    author='Guido Dassori',
+    author_email='guido.dassori@gmail.com',
+    python_requires='>=3.5.2',
+    description='Embeddable minimal asynchronous on disk DB',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    include_package_data=True
+)
