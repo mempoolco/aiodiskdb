f06939448def70999e352c91627775d5c1dc7ba5
guido.dassori@gmail.com <guido.dassori@gmail.com>
2021-06-11 08:23:46 +0200

    versio bump. rel 1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='aiodiskdb',
-    version='1.0a1',
+    version='1.0',
     url='https://github.com/mempoolco/aiodiskdb/',
     license='MIT',
     author='Guido Dassori',
